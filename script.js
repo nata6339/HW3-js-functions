b244aff0545716c87f0b1e853e3cae13a6437294
Nataliia <nata.buchna@gmail.com>
2021-07-01 14:38:51 +0300

    4

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log('------------- #4')
+
+function sum() {
+    let num1 = arguments[0]
+    let num2 = arguments[1]
+
+    console.log(num1 + num2);
+    console.log(num1 - num2);
+    console.log(num1 * num2);
+    console.log(num1 / num2);
+}
+
+sum(10, 5);
+
+
+
+
