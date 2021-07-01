7952b4ace1e8d36e7776058320d3523a38bf0ecd
Nataliia <nata.buchna@gmail.com>
2021-07-01 15:38:12 +0300

    5.6

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-console.log('------------- #4')
+console.log('------------- #4/1')
 
-function sum() {
+function calc() {
     let num1 = arguments[0]
     let num2 = arguments[1]
 
@@ -10,8 +10,64 @@ function sum() {
     console.log(num1 / num2);
 }
 
-sum(10, 5);
+calc(10, 5);
+
+console.log('------------- #4/2')
+
+function calculate(action, ...nums) {
+    let result = 10;
+    nums.forEach(function(num) {
+        switch (action) {
+            case 'sum':
+                result += num;
+                break;
+            case 'sub':
+                result -= num;
+                break;
+            case 'multiply':
+                result *= num;
+                break;
+            case 'div':
+                result /= num;
+                break;
+
+        }
+    });
+
+    return result;
+}
+
+console.log(calculate('sum', 5));
+console.log(calculate('sub', 5));
+console.log(calculate('multiply', 5));
+console.log(calculate('div', 5));
 
 
+
+console.log('------------- #5')
+
+function counter(n) {
+
+    if (n < 6) {
+        console.log(n);
+
+        return counter(n + 1);
+    }
+}
+
+counter(1);
+
+console.log('------------- #6')
+
+function newCounter(n) {
+
+    if (n > 0) {
+        console.log(n);
+
+        return newCounter(n - 1);
+    }
+}
+
+newCounter(5);
 
 
