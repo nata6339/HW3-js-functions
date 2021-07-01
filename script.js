5f030fef0aaa0b8757bacf516c5797fe25ebe67c
Nataliia <nata.buchna@gmail.com>
2021-07-01 15:59:59 +0300

    5-7

--- script.js
+++ script.js
@@ -70,4 +70,15 @@ function newCounter(n) {
 
 newCounter(5);
 
+console.log('------------- #7')
 
+function pow(x, n) {
+    let result = x;
+
+    for (let i = 1; i < n; i++) {
+        result *= x;
+    }
+
+    return result;
+}
+console.log(pow(2, 4));
